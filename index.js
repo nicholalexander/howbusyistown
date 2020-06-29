1537d6cc26d1b6193d293229f1d503850c7df8aa
Nichol Alexander <nichol@goodnation.io>
2020-06-29 07:15:16 -0400

    fonts

--- index.js
+++ index.js
@@ -18,9 +18,53 @@ addEventListener('fetch', event => {
 })
 const someHTML =  `<!DOCTYPE html>
 <html>
+
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+
+  <head>
+    <style>
+      
+      
+      body {
+        background-color: pink;
+        font-family: 'Open Sans', sans-serif;
+      }
+
+      h1 {
+        color: #fafafa;
+        font-size: 6em;
+      }
+
+      p {
+        color: #fafafa;
+      }
+
+      .container {
+          display: flex;           
+          flex-direction: column;  
+          justify-content: center; 
+          align-items: center;
+          height: 100vh;
+      }
+
+      .box {
+          margin: 5px;
+          text-align: center;
+      }
+    </style>
+  </head>
   <body>
-  <h1>Very!</h1>
-  <p>It's always suprisingly busy.  Always.</p>
+    <div class="container">
+
+        <div class="box">
+            <h1> Very! </h1>
+        </div>
+
+        <div class="box">
+            <p>Suprisingly so, as a mater of fact.</p>
+        </div>
+
+    </div>
   </body>
 </html>
 `
